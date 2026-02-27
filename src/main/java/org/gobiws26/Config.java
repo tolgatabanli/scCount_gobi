2aee9db9d677bd2e5b420d4a86ee8c4793c7788d
tabanli <tolgatabanli@gmail.com>
2026-02-27 18:04:29 +0100

    added mode level for gene/transcript counts

--- src/main/java/org/gobiws26/Config.java
+++ src/main/java/org/gobiws26/Config.java
@@ -2,4 +2,10 @@ package org.gobiws26;
 
 public class Config {
     public static int K;
+    public static CountMode COUNT_MODE;
+
+    public enum CountMode {
+        TRANSCRIPT_LEVEL,
+        GENE_LEVEL,
+    }
 }
