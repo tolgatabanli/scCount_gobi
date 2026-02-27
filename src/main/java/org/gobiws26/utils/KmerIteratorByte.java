47fec1a81e925c1ae9ce3eb833d6d24e60fc34d8
tabanli <tolgatabanli@gmail.com>
2026-02-27 18:05:00 +0100

    now is a mainly Debug-purpose class, iterator returning kmers as byte[] windows

--- src/main/java/org/gobiws26/utils/KmerIteratorByte.java
+++ src/main/java/org/gobiws26/utils/KmerIteratorByte.java
@@ -1,83 +1,45 @@
 package org.gobiws26.utils;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
+ * FOR DEBUGGING PROBABLY
  * Takes base sequence as byte array as output from samtools and creates an iterator
- * which returns a long representing kmers of the sequence on each next() call.
+ * which returns kmers of the sequence on each next() call.
  * Note: samtools return a byte array of nucleotides (chars: A:65, C:67, G:71, T:84)
  */
-public class KmerIterator {
+public class KmerIteratorByte implements Iterator<byte[]> {
     private final int K;
     private final byte[] fullSeq;
     private int windowStart = 0;
 
-    private long last = 0;
-    private int lastHash = 0;
-
-    private RollingHash rollingHash = new RollingHash();
-
-    private static byte[] HASH = new byte[256];
-    static {
-        HASH['A'] = 0;
-        HASH['C'] = 1;
-        HASH['G'] = 2;
-        HASH['T'] = 3;
-    }
-
     /**
-     *
-     * @param bases A sequence.
-     * @return a long, representing a kmer of length <= 32. First nucleotide is the biggest bit and
-     * the kmer is placed in the first K least significant bits, in other words, right-aligned when one writes
-     * the binary number where the smallest bit is on the right hand side.
-     */
-    private long convertFirst(byte[] bases) {
-        long kmer = 0L; // 64 bits
-        for (int i = 0; i < K; i++) {
-            kmer <<= 2;
-            kmer |= (HASH[bases[i]] & 3); // &3 for masking (11)
-        }
-        return kmer;
-    }
-
-    /**
-     *
      * @param sequence A sequence.
      * @param k kmer size. Could also use Config.K. Might be removed.
      */
-    public KmerIterator(byte[] sequence, int k) {
-        if (k > sequence.length) throw new IllegalArgumentException("k cannot be larger than sequence length");
+    public KmerIteratorByte(byte[] sequence, int k) {
         if (k <= 0) throw new IllegalArgumentException("k must be positive");
+        if (k > sequence.length) throw new IllegalArgumentException("k cannot be larger than sequence length");
 
-        K = k;
-        fullSeq = sequence;
-        last = convertFirst(fullSeq);
-        windowStart++;
+        this.K = k;
+        this.fullSeq = sequence;
     }
 
-    /**
-     *
-     * @return if there is a kmer that can be returned.
-     */
+    @Override
     public boolean hasNext() {
         return windowStart + K <= fullSeq.length;
     }
 
-    /**
-     *
-     * @return a long representing the next kmer of the sequence.
-     */
-    public long next() {
+    @Override
+    public byte[] next() {
         if (!hasNext()) throw new NoSuchElementException();
-        long current = current << 2;
-        current |= HASH[fullSeq[windowStart + K - 1]];
-
-        if (K < 32) { // set left bits to zero
-            current &= (1L << (2 * K)) - 1;
-        }
-
+        byte[] kmer = Arrays.copyOfRange(fullSeq, windowStart, windowStart + K);
         windowStart++;
-        return current;
+        return kmer;
     }
 }
