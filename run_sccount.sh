68af88be8475252ef37c69e36662ed50571482a2
tabanli <tolgatabanli@gmail.com>
2026-03-12 06:34:52 +0100

    adjusted for simulationg with prot coding genes

--- run_sccount.sh
+++ run_sccount.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 java -jar /home/t/tabanli/Desktop/scCount/out/artifacts/scCount_jar/scCount.jar count \
-	-o /home/t/tabanli/Desktop/scCount/out/count_out_simul_noMut \
+	-o /home/t/tabanli/Desktop/scCount/out/count_out_simul_noMut_gamma_protcoding \
 	-idx /home/t/tabanli/Desktop/scCount/out/sccount_utrplus.idx \
-	-r2 /mnt/cip/home/t/tabanli/Desktop/scCount/simulation/out_noMut/read2.fastq \
+	-r2 /mnt/cip/home/t/tabanli/Desktop/scCount/simulation/out_noMut_gamma_protcoding/read2.fastq \
 	-batchSize 500000 \
-	-threads 8
+	-threads 30
