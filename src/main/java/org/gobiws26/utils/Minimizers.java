2cdb54011a72ee55e390d1674fd3486b9d785041
tabanli <tolgatabanli@gmail.com>
2026-03-13 16:35:14 +0100

    removed quality bits from minimizers that are now 8mers

--- src/main/java/org/gobiws26/utils/Minimizers.java
+++ src/main/java/org/gobiws26/utils/Minimizers.java
@@ -6,49 +6,36 @@ import org.gobiws26.Config;
 public class Minimizers {
     public static ShortArrayList of(byte[] bases, byte[] phredSeq) {
         KmerIteratorLong kiLong = new KmerIteratorLong(bases, phredSeq, Config.K);
-        ShortArrayList minimSet = new ShortArrayList();
+        ShortArrayList minimList = new ShortArrayList();
 
-        // For each Kmer in the read, find the minimizer and store pairwise different minimizers with the worst found quality assigned from kmers
+        // For each Kmer in the read, find the minimizer
         short currentMinim = getMinimizer(kiLong.nextLong());
-        byte worstQualityLastMinim = (byte) ((currentMinim >>> 14) & 0b11); // quality score ordering: 11 > 10 > 01 > 00; TODO: will decide what all may correspond to (11 is normal, no 'N' etc.)
         short lastFoundMinim = currentMinim;
-        //int windowSize = Config.K - 7; // to consider "real" sequential duplicates, 7 magic number is okay since minimizers are 7mer since short.
-        while(kiLong.hasNext()) {
+
+        while (kiLong.hasNext()) {
             currentMinim = getMinimizer(kiLong.nextLong());
-
-            byte currentQualityBits = (byte) ((currentMinim >>> 14) & 0b11); // unsigned shift!
-            if (((currentMinim & 0x3FFF) != (lastFoundMinim & 0x3FFF))) { // only add when we change the minimizer sequence, so that we're sure the qualities are considered correctly // || windowSize == 0
-                lastFoundMinim = (short) ((lastFoundMinim & 0x3FFF) | (worstQualityLastMinim << 14)); // 'infect' with worst kmer's quality
-
-                minimSet.add(lastFoundMinim);
-
+            if (currentMinim != lastFoundMinim) {
+                minimList.add(lastFoundMinim);
                 lastFoundMinim = currentMinim;
-                //windowSize = Config.K - 7;
-                worstQualityLastMinim = currentQualityBits;
-            } else {
-                if (worstQualityLastMinim > currentQualityBits) {
-                    worstQualityLastMinim = currentQualityBits;
-                }
-                //windowSize--;
             }
         }
-        lastFoundMinim = (short) ((lastFoundMinim & 0x3FFF) | (worstQualityLastMinim << 14));
 
-        minimSet.add(lastFoundMinim); // flush
-        return minimSet;
+        minimList.add(lastFoundMinim); // flush
+        return minimList;
     }
 
-    private static final short gmerMask = 0x3FFF; // take the rightmost 14 bits
+    private static final int gmerMask = 0xFFFF;
     public static short getMinimizer(long kmer) {
-        short biggest = gmerMask;
-        byte qualityBits = (byte) (kmer >>> (Config.K * 2));
-        for (int i = 0; i < Config.K - 7 + 1; i++) {
-            short currentGmer = (short) (kmer & gmerMask);
-            if (currentGmer < biggest) biggest = currentGmer;
+        int windows = Config.K - 8 + 1;
+        if (windows < 1) windows = 1;
 
+        int smallest = gmerMask;
+        for (int i = 0; i < windows; i++) {
+            int currentGmer = (int) (kmer & gmerMask);
+            if (currentGmer < smallest) smallest = currentGmer;
             kmer >>>= 2;
         }
-        biggest |= (short) (qualityBits << 14); // 'infect' the quality from kmer to gmer
-        return biggest;
+
+        return (short) smallest;
     }
 }
