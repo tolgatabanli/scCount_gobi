12ab387ec3617bf62d140ae17312c61cb7492ae3
tabanli <tolgatabanli@gmail.com>
2026-03-18 13:42:31 +0100

    updated call

--- run_index.sh
+++ run_index.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-java -jar /home/t/tabanli/Desktop/scCount/out/artifacts/scCount_jar/scCount.jar index \
+java -jar /mnt/cip/home/t/tabanli/Desktop/scCount/out/artifacts/miniqut3_jar/miniqut3.jar index \
 	-f /mnt/raidbio2/extdata/praktikum/genprakt/genprakt-ws25/Block/pig-genome/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa.gz \
 	-g /mnt/raidbio2/extdata/praktikum/genprakt/genprakt-ws25/Block/pig-genome/Sus_scrofa.Sscrofa11.1.115.chr.gtf.gz \
-	-k 20 \
-	-idx /mnt/cip/home/t/tabanli/Desktop/scCount/out/sccount_utrplus.idx
+	-k 30 -minimLength 12 \
+	-idx /mnt/cip/home/t/tabanli/Desktop/scCount/out/miniqut3_k30_g12.idx
 
